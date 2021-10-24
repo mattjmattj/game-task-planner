bbaf18210fd916dd05924ce03afe06223607fc5d
Matthias Jouan <matthias.jouan@gmail.com>
2021-10-25 00:15:21 +0200

    feat(#tdd): invalidate when someone is given several tasks

--- src/Validator/AssignementValidator.php
+++ src/Validator/AssignementValidator.php
@@ -41,6 +41,17 @@ class AssignementValidator extends ConstraintValidator
                 // in order to keep errors under control
                 break;
             }
+
+            if ($this->isAssigneeDuplicated($task, $assignement->getTasks())) {
+                $this->context->buildViolation($constraint->message)
+                    ->setCode(Assignement::MULTIPLE_TASKS_ERROR)
+                    ->addViolation();
+                
+                // duplication errors can get messy, so we break at the first one
+                // in order to keep errors under control
+                break;
+            }
+
         }
     }
 
@@ -53,6 +64,21 @@ class AssignementValidator extends ConstraintValidator
             }
             if ($task->getGame() === $comparedTask->getGame()
                 && $task->getType() === $comparedTask->getType()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private function isAssigneeDuplicated(Task $task, iterable $tasks): bool
+    {
+        foreach ($tasks as $comparedTask) {
+            /** @var Task $comparedTask */
+            if ($task === $comparedTask) {
+                continue;
+            }
+            if ($task->getGame() === $comparedTask->getGame()
+                && $task->getAssignee() === $comparedTask->getAssignee()) {
                 return true;
             }
         }
