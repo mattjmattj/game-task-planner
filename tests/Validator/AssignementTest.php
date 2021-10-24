d9a8bb1a9ee87344f93f57b9842bbfa8ceb2d667
Matthias Jouan <matthias.jouan@gmail.com>
2021-10-24 23:19:58 +0200

    feat(#tdd): test Assignement's most straightfwd case
    We make some changes in the test case structure, as we take advantage of
    Symfony's ConstraintValidatorTestCase.
    This test is green, since an empty validator validates everything.
    Nevertheless, we need this first test since it is the base contract

--- tests/Validator/AssignementTest.php
+++ tests/Validator/AssignementTest.php
@@ -5,20 +5,31 @@ namespace App\Tests\Validator;
 use App\Entity\Assignement;
 use App\Entity\Person;
 use App\Entity\Planning;
+use App\Entity\Task;
 use App\Entity\TaskType;
+use App\Validator\Assignement as AssignementConstraint;
+use App\Validator\AssignementValidator as AssignementValidator;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Validator\Test\ConstraintValidatorTestCase;
 
-final class AssignementTest extends TestCase
+final class AssignementTest extends ConstraintValidatorTestCase
 {
+    public function createValidator()
+    {
+        return new AssignementValidator();
+    }
+
     private function makePlanning(): Planning
     {
         $planning = new Planning;
 
+        $planning->setTitle('Test planning');
+        $planning->setGameNumber(3);
         $planning->addTaskType((new TaskType)->setName('type 1'));
         $planning->addTaskType((new TaskType)->setName('type 2'));
-        $planning->addTaskType((new TaskType)->setName('type 3'));
         $planning->addPerson((new Person)->setName('person 1'));
         $planning->addPerson((new Person)->setName('person 2'));
+        $planning->addPerson((new Person)->setName('person 3'));
 
         return $planning;
     }
@@ -37,8 +48,33 @@ final class AssignementTest extends TestCase
      * @test
      */
     public function shouldValidateAPerfectAssignement(): void
-    {        
-        $this->markTestIncomplete();
+    {
+        $planning = $this->makePlanning();
+        $persons = $planning->getPersons();
+        $taskTypes = $planning->getTaskTypes();
+        $assignement = $this->makeAssignement($planning);
+
+        $assignement->addTask(
+            (new Task)->setAssignee($persons[0])->setType($taskTypes[0])->setGame(0)
+        );
+        $assignement->addTask(
+            (new Task)->setAssignee($persons[1])->setType($taskTypes[1])->setGame(0)
+        );
+        $assignement->addTask(
+            (new Task)->setAssignee($persons[1])->setType($taskTypes[0])->setGame(1)
+        );
+        $assignement->addTask(
+            (new Task)->setAssignee($persons[2])->setType($taskTypes[1])->setGame(1)
+        );
+        $assignement->addTask(
+            (new Task)->setAssignee($persons[2])->setType($taskTypes[0])->setGame(2)
+        );
+        $assignement->addTask(
+            (new Task)->setAssignee($persons[0])->setType($taskTypes[1])->setGame(2)
+        );
+
+        $this->validator->validate($assignement, new AssignementConstraint);
+        $this->assertNoViolation();
     }
 
     /**
