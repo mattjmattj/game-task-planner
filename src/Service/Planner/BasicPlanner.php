909a9fa336c3a03195c0676cb916c93b8accbb2c
Matthias Jouan <matthias.jouan@gmail.com>
2021-10-25 21:23:02 +0200

    feat(#tdd): implement the too-many-tasks case

--- src/Service/Planner/BasicPlanner.php
+++ src/Service/Planner/BasicPlanner.php
@@ -25,14 +25,38 @@ final class BasicPlanner implements PlannerInterface
         if (0 === count($types)) {
             return $assignement;
         }
+
+        // we'll use a priority queue initialized with every person having a priority
+        // equal to the number of games. Every time a task is assigned to somebody
+        // their priority is decreased, making them the last pick
+        $peopleQueue = new \SplPriorityQueue;
+        $peopleQueue->setExtractFlags(\SplPriorityQueue::EXTR_BOTH);
+        foreach ($persons as $person) {
+            $peopleQueue->insert($person, $nbGames);
+        }
+        
         for ($game = 0 ; $game < $nbGames ; $game++) {
-            foreach($types as $k => $type) {
+            // for every game, we have to keep track of who had a task assigned
+            // and put them back in the queue only once all the tasks have been
+            // assigned for the game
+            $selectedPeople = [];
+            foreach($types as $type) {
+                $queueTop = $peopleQueue->extract();
+                $assignee = $queueTop['data'];
+                $priority = $queueTop['priority'];
+
                 $assignement->addTask(
                     (new Task)
-                        ->setAssignee($persons[$k])
+                        ->setAssignee($assignee)
                         ->setType($type)
                         ->setGame($game)
                 );
+
+                $selectedPeople[] = [$assignee, $priority - 1];
+            }
+
+            foreach($selectedPeople as list($person, $priority)) {
+                $peopleQueue->insert($person, $priority);
             }
         }
 
