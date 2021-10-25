c31e2a3613837424a56b0dc418a90708840a7bcf
Matthias Jouan <matthias.jouan@gmail.com>
2021-10-25 18:46:20 +0200

    feat(#tdd): implements the basic tests for a Planner
    very naive solution, but test is green

--- src/Service/Planner/BasicPlanner.php
+++ src/Service/Planner/BasicPlanner.php
@@ -2,7 +2,38 @@
 
 namespace App\Service\Planner;
 
+use App\Entity\Assignement;
+use App\Entity\Planning;
+use App\Entity\Task;
+
 final class BasicPlanner implements PlannerInterface
 {
-    
+    public function makeAssignement(Planning $planning): Assignement
+    {
+        $assignement = new Assignement;
+
+        $assignement->setPlanning($planning);
+
+        $persons = $planning->getPersons();
+        $types = $planning->getTaskTypes();
+        $nbGames = $planning->getGameCount();
+
+        if (count($persons) < count($types)) {
+            throw new ImpossiblePlanningException("Not enough people for the given task types.");
+        }
+
+        // naive algorithm
+        for ($game = 0 ; $game < $nbGames ; $game++) {
+            foreach($types as $k => $type) {
+                $assignement->addTask(
+                    (new Task)
+                        ->setAssignee($persons[$k])
+                        ->setType($type)
+                        ->setGame($game)
+                );
+            }
+        }
+
+        return $assignement;
+    }
 }
