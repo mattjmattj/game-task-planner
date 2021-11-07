1320478aa8fddf22cd7d14e5c744b506a94cb165
Matthias Jouan <matthias.jouan@gmail.com>
2021-11-07 21:52:12 +0100

    feat: compute an assignment from planning page

--- src/Controller/PlanningCrudController.php
+++ src/Controller/PlanningCrudController.php
@@ -2,16 +2,35 @@
 
 namespace App\Controller;
 
+use App\Backtracking\Constraint\AssignmentValidatorConstraint;
+use App\Backtracking\Constraint\NoSpecialistConstraint;
+use App\Backtracking\Constraint\NotTooManyTasksConstraint;
 use App\Entity\Planning;
+use App\Service\Planner\BacktrackingPlanner;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class PlanningCrudController extends AbstractCrudController
 {
+    public function __construct(
+        private BacktrackingPlanner $planner,
+        ValidatorInterface $validator
+    )
+    {
+        $this->planner->addConstraint(new AssignmentValidatorConstraint($validator));
+        $this->planner->addConstraint(new NotTooManyTasksConstraint);
+        $this->planner->addConstraint(new NoSpecialistConstraint);
+    }
+
     public static function getEntityFqcn(): string
     {
         return Planning::class;
@@ -28,4 +47,34 @@ class PlanningCrudController extends AbstractCrudController
         ];
     }
     
+    public function configureActions(Actions $actions): Actions
+    {
+        $actions->add(
+            Crud::PAGE_EDIT,
+            Action::new('makeAssignment', 'Compute a planning', 'fa fa-calendar-alt')
+                ->linkToCrudAction('makeAssignment')
+        );
+
+        $actions->add(
+            Crud::PAGE_INDEX,
+            Action::new('makeAssignment', 'Compute a planning')
+                ->linkToCrudAction('makeAssignment')
+        );
+
+        return $actions;
+    }
+
+    public function makeAssignment(AdminContext $context)
+    {
+        $planning = $context->getEntity()->getInstance();
+        $assignment = $this->planner->makeAssignment($planning);
+
+        return $this->render(
+            'admin/assignment.html.twig',
+            [
+                'assignment' => $assignment->toArray(),
+                'planning' => $planning
+            ]
+        );
+    }
 }
